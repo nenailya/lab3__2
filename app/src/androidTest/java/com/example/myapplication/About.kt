cf8ab05283b0a8c0843f7edf6535fb4ad72441cf
n_zgr <nzgr1@mail.ru>
2021-11-14 20:30:36 +0300

    initial commit

---
+++ app/src/androidTest/java/com/example/myapplication/About.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.view.View
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeRight
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matcher
+
+
+fun openAbout(){
+    //onView(withId(R.id.nav_view)).perform(click())
+    //onView(withId(R.id.to_about)).perform(click())
+
+
+}
+
