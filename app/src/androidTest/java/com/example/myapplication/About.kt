aae67a57317ad51d2071ae71d3ef05b0c7d6dbbe
n_zgr <nzgr1@mail.ru>
2021-12-05 21:52:33 +0300

    initial commit

--- app/src/androidTest/java/com/example/myapplication/About.kt
+++ app/src/androidTest/java/com/example/myapplication/About.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.view.Gravity
 import android.view.View
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
@@ -7,6 +8,14 @@ import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.swipeRight
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerActions.open
+import androidx.test.espresso.contrib.DrawerMatchers
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions
+import androidx.test.espresso.contrib.NavigationViewActions.navigateTo
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.hamcrest.Matcher
@@ -16,6 +25,14 @@ fun openAbout(){
     //onView(withId(R.id.nav_view)).perform(click())
     //onView(withId(R.id.to_about)).perform(click())
 
+}
 
+fun About(){
+    onView(withId(R.id.drawer_layout))
+        .check(matches(isClosed(Gravity.LEFT)))
+        .perform(open())
+
+    onView(withId(R.id.drawer_nav_view))
+        .perform(navigateTo(R.id.activity_about))
 }
 
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.Activity1Binding
@@ -11,7 +12,10 @@ import androidx.navigation.NavController
 
 
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -19,13 +23,22 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.*
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.DrawerActions
+import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions.navigateTo
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.viewbinding.ViewBinding
 import com.example.myapplication.databinding.ActivityMainBinding
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
     private lateinit var binding: Activity1Binding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,14 +46,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.bnToSecond.setOnClickListener { goToSecond() }
 
-        navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+        //com.example.myapplication.About()
 
-        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawer_layout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.drawerNavView.setupWithNavController(navController)
+
+
+        //navController = (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment).navController
+
+        //  drawer_nav_view.setNavigationItemSelectedListener {
+        //      if (it.itemId == R.id.to_about) {
+        //          drawer_layout.closeDrawer(GravityCompat.START, true)
+        //          launchActivity<activityAbout>()
+        //          //navController.navigate(R.id.action_to_activityAbout)
+        //      }
+        //      true
+        //  }
+
+        //  val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawer_layout)
+        //  setupActionBarWithNavController(navController, appBarConfiguration)
+        //  binding.drawerNavView.setupWithNavController(navController)
 
     }
-
 
     private fun goToSecond() {
         startActivity(Intent(this, activity2::class.java))
